3d7efc2fb2a6135fff7065322efc9e12864851b9
Ora Vrapcani <oravrapcani@gmail.com>
2021-05-16 14:06:38 +0200

    created console project

---
+++ SteganografiaCon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteganografiaCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
